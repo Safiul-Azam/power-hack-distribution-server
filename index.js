8937598f0c0b6b3952e4ef85f1d96456e9c098f6
mdsafiulazam <mdriadkhan20@gmail.com>
2022-06-30 18:21:50 +0600

    billing information get with get method

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//port set
 const port = process.env.PORT || 5000
-
 require('dotenv').config()
+
+// middle ware
 app.use(cors())
 app.use(express.json())
+
+// connect mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5d2zd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+//FUNCTION FOR API
+async function run(){
+    try{
+
+        await client.connect()
+        const billCollection = client.db('power-hack').collection('billingList')
+    
+    
+        app.get('/billing-list', async (req, res)=>{
+            const billList = await billCollection.find().toArray()
+            res.send(billList)
+        })
+
+        app.post('/add-billing', async(req, res)=>{
+            const billDoc = req.body
+            const result = await billCollection.insertOne(billDoc)
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 
 
 
